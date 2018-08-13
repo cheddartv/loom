dc1502f275c8431f723c981309e6934374e6e73a
Ross Cooperman <cooperman@gmail.com>
2018-08-13 13:11:58 -0400

    Add config file parsing via Viper

--- main.go
+++ main.go
@@ -1,13 +1,40 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/spf13/viper"
 )
 
+type manifest struct {
+	Output string
+	Inputs []string
+}
+
+type config struct {
+	Manifests []manifest
+}
+
 type Context struct {
+	Config config
 }
 
 func main() {
 	viper.SetConfigName("loom")
+
 	viper.AddConfigPath("/etc/")
+	viper.AddConfigPath("./etc")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error reading config file: %s \n", err))
+	}
+
+	var context Context
+	err = viper.Unmarshal(&context.Config)
+	if err != nil {
+		panic(fmt.Errorf("Fatal error reading config file: %s \n", err))
+	}
+
+	fmt.Printf("Got an output: %v\n", context.Config)
 }
