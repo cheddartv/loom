8604521fc58312c0f399a742f6595e94170271e8
Ross Cooperman <cooperman@gmail.com>
2018-08-14 10:42:39 -0400

    Add tests for input and output parsing

--- config/load_test.go
+++ config/load_test.go
@@ -16,4 +16,12 @@ var _ = Describe("loading configuration file", func() {
 	It("creates the correct number of manifests", func() {
 		Expect(len(c.Manifests)).To(Equal(2))
 	})
+
+	It("configures the correct output", func() {
+		Expect(c.Manifests[0].Output).To(Equal("a.m3u8"))
+	})
+
+	It("configures the correct inputs", func() {
+		Expect(c.Manifests[0].Inputs).To(Equal([]string{"a/primary.m3u8", "a/backup.m3u8"}))
+	})
 })
